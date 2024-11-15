d699933cf2fe428df76f48a025ef07746a4fa923
soryaek <sorya.ek@gmail.com>
2024-11-15 13:53:04 -0800

    update mongoose schema

--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -4,7 +4,9 @@ const Post = new mongoose.Schema({
     name: { type: String, required: true},
     prompt: { type: String, required: true},
     photo: { type: String, required: true}
-})
+}, { timestamps: true })
+
+Post.index({ createdAt: -1 });
 
 const PostScheme = mongoose.model('Post', Post);
 
